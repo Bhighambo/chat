6213fb218931c92e1c146bab3445974a49fb27a5
Bhighambo <jacksonkennedy336@gmail.com>
2023-12-23 13:32:36 +0100

    la modification de la page d'index et menu

--- menu.php
+++ menu.php
@@ -42,20 +42,14 @@ https://templatemo.com/tm-580-woox-travel
       </div>
     </div>
   </div>
-  <!-- ***** Preloader End ***** -->
-
-  <!-- ***** Header Area Start ***** -->
   <header class="header-area header-sticky">
     <div class="container">
         <div class="row">
             <div class="col-12">
                 <nav class="main-nav">
-                    <!-- ***** Logo Start ***** -->
                     <a href="index.html" class="logo">
                         <img src="assets/images/logo.png" alt="">
                     </a>
-                    <!-- ***** Logo End ***** -->
-                    <!-- ***** Menu Start ***** -->
                     <ul class="nav">
                         <li><a href="index.html" class="active">Home</a></li>
                         <li><a href="about.html">About</a></li>
@@ -66,7 +60,6 @@ https://templatemo.com/tm-580-woox-travel
                     <a class='menu-trigger'>
                         <span>Menu</span>
                     </a>
-                    <!-- ***** Menu End ***** -->
                 </nav>
             </div>
         </div>
