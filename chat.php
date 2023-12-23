9504d2e2a74c68411639abc35fee8063f68c2857
Bhighambo <jacksonkennedy336@gmail.com>
2023-12-23 21:44:57 +0100

    modification

--- chat.php
+++ chat.php
@@ -44,29 +44,41 @@
                     </div>
                     </div>
 
-                    <div class="row sideBar">
-                    <div class="row sideBar-body">
-                        <div class="col-sm-3 col-xs-3 sideBar-avatar">
-                        <div class="avatar-icon">
-                            <img src="https://bootdey.com/img/Content/avatar/avatar1.png">
-                        </div>
-                        </div>
-                        <div class="col-sm-9 col-xs-9 sideBar-main">
-                        <div class="row">
-                            <div class="col-sm-8 col-xs-8 sideBar-name">
-                            <span class="name-meta">John Doe
-                            </span>
-                            </div>
-                            <div class="col-sm-4 col-xs-4 pull-right sideBar-time">
-                            <span class="time-meta pull-right">18:18
-                            </span>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
+                    <?php 
 
-                
-                    </div>
+                        $recupUser = $bdd->prepare("SELECT * FROM membre");
+                        $recupUser->execute();
+                        while($donnees_infos = $recupUser->fetch()){
+                        ?>
+                        <div class="row sideBar">
+                            <div class="row sideBar-body">
+                                <div class="col-sm-3 col-xs-3 sideBar-avatar">
+                                    <div class="avatar-icon">
+                                        <img src="https://bootdey.com/img/Content/avatar/avatar1.png">
+                                    </div>
+                                </div>
+                                <div class="col-sm-9 col-xs-9 sideBar-main">
+                                    <div class="row">
+                                        <div class="col-sm-8 col-xs-8 sideBar-name">
+                                            <span class="name-meta"><?php echo $donnees_infos->nom." ".$donnees_infos->postnom; ?>
+                                            </span>
+                                        </div>
+                                        <div class="col-sm-4 col-xs-4 pull-right sideBar-time">
+                                            <span class="time-meta pull-right">21:23
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <?php
+
+                    }
+
+                     ?>
+
+                    
                 </div>
 
                 <div class="side-two">
